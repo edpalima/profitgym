81888ac7a6e274b7436c9ef0cc24624996a9e93d
edpalima <edwindpalima@gmail.com>
2025-04-27 09:53:16 +0800

    update migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'member', 'staff'])->default('member');
+            $table->enum('role', ['ADMIN', 'MEMBER', 'STAFF'])->default('MEMBER');
             $table->rememberToken();
             $table->timestamps();
         });
