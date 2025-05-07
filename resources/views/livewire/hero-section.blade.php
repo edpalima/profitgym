6b6603d6696f83bc3b34549dabeffbc428d8c384
edpalima <edwindpalima@gmail.com>
2025-05-07 11:25:17 +0800

    add overlay on hero carousel

--- resources/views/livewire/hero-section.blade.php
+++ resources/views/livewire/hero-section.blade.php
@@ -1,28 +1,30 @@
-
+<div>
     <!-- Hero Section Begin -->
     <section class="hero-section">
         <div class="hs-slider owl-carousel">
             <div class="hs-item set-bg" data-setbg="img/hero/hero-5.jpg">
+                <div class="bg-overlay"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-6 offset-lg-6">
                             <div class="hi-text">
                                 <span>Shape your body</span>
                                 <h1>Be <strong>strong</strong> traning hard</h1>
-                                <a href="{{ route('memberships.index')}}" class="primary-btn">Join Now</a>
+                                <a href="{{ route('memberships.index') }}" class="primary-btn">Join Now</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="hs-item set-bg" data-setbg="img/hero/hero-4.jpg">
+                <div class="bg-overlay"></div>
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-6 offset-lg-6">
                             <div class="hi-text">
                                 <span>Shape your body</span>
                                 <h1>Be <strong>strong</strong> traning hard</h1>
-                                <a href="{{ route('memberships.index')}}" class="primary-btn">Join Now</a>
+                                <a href="{{ route('memberships.index') }}" class="primary-btn">Join Now</a>
                             </div>
                         </div>
                     </div>
@@ -30,3 +32,4 @@
             </div>
         </div>
     </section>
+</div>
